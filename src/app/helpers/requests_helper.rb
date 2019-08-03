76fda42e03b6ea84dd519155ede2e5a503a99001
Honda Hiroki <hiro_4455@hotmail.com>
2019-08-03 14:05:02 +0900

    Change color of requesting,reject and approved

--- src/app/helpers/requests_helper.rb
+++ src/app/helpers/requests_helper.rb
@@ -52,6 +52,18 @@ module RequestsHelper
     workflow.approved ? "済":"却下"
   end
 
+  def status_class status
+    case status
+      when '申請中'
+        return 'processing'
+      when '承認'
+        return 'approved'
+      when '却下'
+        return 'reject'
+    end
+    'processing'
+  end
+
   def extract_placeholder name
     case name
       when '{CurrentUser}'
